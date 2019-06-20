d7ec7d4c53d3b29086680ca8a1da02f6ba5c5c3a
asyncnc-lee <1156553634@qq.com>
2019-06-20 11:24:52 +0800

    *富文本联系

---
+++ src/components/RichText.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="rich-text">
+    <div class="tools">
+      <button @click="execCommand('bold')">加粗</button>
+      <button @click="execCommand('createLink')">链接</button>
+    </div>
+    <div class="content" contenteditable="true"></div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'RichText',
+    data () {
+      return {
+      }
+    },
+    methods: {
+      execCommand (aCommandName = '', aValueArgument = null) {
+        let bool = document.execCommand(aCommandName, false, aValueArgument)
+        console.log(bool)
+      }
+    }
+  }
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .rich-text .tools {
+    text-align: left;
+  }
+  .rich-text .tools b {
+    cursor: pointer;
+  }
+  .rich-text .content{
+    border: 1px solid #ddd;
+    height: 300px;
+    width: 800px;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    text-align: left;
+    padding: 10px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import ScrollTest from '@/components/ScrollTest'
 import Address from '@/components/Address'
+import RichText from '@/components/RichText'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
       path: '/address',
       name: 'Address',
       component: Address
+    },
+    {
+      path: '/richText',
+      name: 'RichText',
+      component: RichText
     }
   ]
 })
